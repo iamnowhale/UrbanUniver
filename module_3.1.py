64c8f34982b3cc4384574aae0dcae2ddd9db3d83
iamnowhale <131859906+iamnowhale@users.noreply.github.com>
2024-09-25 22:32:35 +0300

    Add files via upload

---
+++ module_3.1.py
@@ -0,0 +1,26 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    length = len(string)
+    upper_case = string.upper()
+    lower_case = string.lower()
+    return (length, upper_case, lower_case)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string_lower = string.lower()
+    for item in list_to_search:
+        if item.lower() == string_lower:
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
