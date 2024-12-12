4ad56b66458b1ce6062b51e5439e95d3ad630504
iamnowhale <131859906+iamnowhale@users.noreply.github.com>
2024-12-12 23:02:55 +0300

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -1,20 +1,16 @@
 def is_prime(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-
-
-        def check_prime(n):
-            if n < 2:
-                return False
-            for i in range(2, int(n ** 0.5) + 1):
-                if n % i == 0:
-                    return False
-            return True
-
-        if check_prime(result):
-            print("Простое")
-        else:
+        
+        if result < 2:
             print("Составное")
+        else:
+            for i in range(2, int(result ** 0.5) + 1):
+                if result % i == 0:
+                    print("Составное")
+                    break
+            else:
+                print("Простое")
 
         return result
 
@@ -24,8 +20,6 @@ def is_prime(func):
 @is_prime
 def sum_three(a, b, c):
     return a + b + c
-
-
 
 result = sum_three(2, 3, 6)
 print(result)
